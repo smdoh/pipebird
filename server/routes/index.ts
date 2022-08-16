cb3064dc0f618484025cc27851e2a927a2bd0377
Ian Edwards <ian@realizefi.com>
2022-08-16 15:28:08 -0400

    feat: init schema

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { accountsRouter } from "./accounts.js";
 import { healthzRouter } from "./healthz.js";
 
 const indexRouter = Router();
 
 indexRouter.use("/healthz", healthzRouter);
-indexRouter.use("/accounts", accountsRouter);
 
 export { indexRouter };
